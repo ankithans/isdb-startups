8e8df4762905ea3818c70dc6c7415a9d3bd7fa99
Ankit Hans <ankithans1947@gmail.com>
2021-01-17 16:37:29 +0530

    feat(client): replace lorem

--- client/components/DashboardHeader.js
+++ client/components/DashboardHeader.js
@@ -12,10 +12,12 @@ export default function DashboardHeader() {
             <div className='h-1 w-20 bg-indigo-500 rounded'></div>
           </div>
           <p className='lg:w-1/2 w-full leading-relaxed text-gray-500'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-            quisquam at illo, veniam odit quam nihil rem provident non. Hic
-            dolores praesentium repudiandae soluta doloribus atque
-            necessitatibus! Libero, ullam soluta!
+            Entrepreneurs and Investors face lot of dilemma before starting a
+            startup. As we all know that starting a venture is not an easy task,
+            there are lot of challenges associated with it. To help our
+            investors to overcome this dilemma we have designed this website. We
+            have used various ml model so that they can know beforehand whether
+            the startup they are starting would be beneficial for them or not.
           </p>
         </div>
       </div>
